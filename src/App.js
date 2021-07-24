ea23ebff9f671fc9d095855864629d21df9ddf9c
Karan Mann <karan.mann@outlook.com>
2021-07-24 14:39:27 +0200

    Store created and material ui code added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { Provider } from 'react-redux'
 import React from  'react'
 import moment from 'moment'
+import store from './redux/store'
 export const App = () => {
   return (
-    <div className="App">
-      <h1>{moment().format('LLLL')}</h1>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>{moment().format('LLLL')}</h1>
+      </div>
+    </Provider>
+    
   );
 }
-
-export default App;
