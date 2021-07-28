be87aa2e4ca6dd6d18a5e75044430328c94335a4
Karan Mann <karan.mann@outlook.com>
2021-07-28 22:55:10 +0200

    Todo app using redux toolkit completed

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,15 +1,32 @@
+import { Button, Card, Typography } from '@material-ui/core'
 import { useDispatch, useSelector } from 'react-redux'
 
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline'
 import Checkbox from '@material-ui/core/Checkbox'
-import Clear from '@material-ui/icons/Clear'
-import IconButton from '@material-ui/core/IconButton'
+import DeleteIcon from '@material-ui/icons/Delete'
 import React from 'react'
-import Star from '@material-ui/icons/Star'
-import StarBorder from '@material-ui/icons/StarBorder'
+import Tooltip from '@material-ui/core/Tooltip';
+import moment from 'moment'
+import styled from 'styled-components'
 import todos from '../reducers/todos'
 
-const style = { color: '#FFC700', fontSize: '1.5em' };
+const done = { color: '#00FF27'}
+const notDone = { color: '#778899'}
 const otherStyle = { color: 'white', fontSize: '1.5em' };
+
+const TaskListCard = styled(Card)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 90%;
+  padding: 10px;
+  margin: 20px 0 10px 0
+`
+
+const LineThrough = styled(Typography)`
+  text-decoration: line-through
+`
 const TodosList = () => {
   const tasks = useSelector((store) => store.todos.tasks)
   const completedTasks = tasks.filter(singleTask => singleTask.isComplete)
@@ -20,24 +37,47 @@ const TodosList = () => {
 
   return (
     <>
-      <h4>Completed Tasks: {completedTasks.length}/{tasks.length} </h4>
+      <Typography variant='button'>Completed Tasks: {completedTasks.length}/{tasks.length} </Typography>
       { tasks.map(singleTask => (
-        <div>
-          <Checkbox
-            icon={<StarBorder style={style} />}
-            checkedIcon={<Star style={style} />}
-            type="checkbox"
-            checked={singleTask.isComplete}
-            onChange={() => dispatch(todos.actions.toggleComplete(singleTask.id))}
-          />
-          <p>{singleTask.description}</p>
-          <IconButton onClick={() => dispatch(todos.actions.removeTodo(singleTask.id))}>
-            Edit
-          </IconButton>
-          <IconButton onClick={() => dispatch(todos.actions.removeTodo(singleTask.id))}>
-            <Clear style={otherStyle} />
-          </IconButton>
-        </div>
+        <TaskListCard variant="outlined">
+          <Tooltip title='Mark status'>
+            <Checkbox
+              icon={<CheckCircleOutlineIcon style={notDone} />}
+              checkedIcon={<CheckCircleOutlineIcon style={done} />}
+              type="checkbox"
+              checked={singleTask.isComplete}
+              onChange={() => dispatch(todos.actions.toggleComplete(singleTask.id))}
+            />
+          </Tooltip>
+          <div>
+          
+            <Tooltip title={`Added: ${moment().calendar()}`}>
+            {singleTask.isComplete ?
+              <LineThrough
+                variant='h6'
+                color='textSecondary'
+                gutterBottom
+              >{singleTask.description}
+              </LineThrough>
+             :
+              <Typography
+                variant='h6'
+                color='textSecondary'
+                gutterBottom
+              >{singleTask.description}
+              </Typography>
+            }
+            </Tooltip> 
+          </div>
+          <Tooltip title='Delete todo'>
+            <Button
+              variant="contained" 
+              color="primary"
+              onClick={() => dispatch(todos.actions.removeTodo(singleTask.id))}>
+              <DeleteIcon style={otherStyle} />
+            </Button>
+          </Tooltip>
+        </TaskListCard>
       ))}
     </>
   )
